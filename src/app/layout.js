a87e2051ea6cebe0299cde0018d88c8161fd9bdd
Aman Pal <palaman2626@gmail.com>
2025-11-18 11:09:33 +0530

    modified the adsense script

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="bg-gray-100 text-gray-900">
 
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6004304352641395"
+      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6004304352641395"
+      strategy="afterInteractive"
      crossorigin="anonymous"></Script>
 
         <Navbar />
